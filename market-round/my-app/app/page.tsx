c5234e619afb9cced71fafda48f381add4f8d675
Your Name <nishan.mayadunna@gmail.com>
2026-08-09 06:35:59 +0530

    1st commit

--- market-round/my-app/app/page.tsx
+++ market-round/my-app/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
             To get started, edit the{" "}
             <code className="rounded bg-black/[.06] px-1.5 py-0.5 font-mono text-[0.9em] dark:bg-white/[.08]">
               page.tsx
-            </code>{" "}
+             </code>{" "}
             file.
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
